18c98682ebb75441681d1f853d981bec9c497654
Iegor Azuaga <iegorazuaga@gmail.com>
2019-04-20 12:06:39 +0200

    implement pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  apps: [
+    {
+      name: 'API Gateway',
+      script: 'gateway/index.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    },
+    {
+      name: 'Availability Worker',
+      script: 'workers/product-availability.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    },
+    {
+      name: 'Budget Worker',
+      script: 'workers/budget.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+}
